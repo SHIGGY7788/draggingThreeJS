5888e9796d5b0814464a0487cab9e4c4f015d0a5
Evan Edwards <40186280+SHIGGY7788@users.noreply.github.com>
2024-04-11 14:12:24 -0700

    upd

--- main.js
+++ main.js
@@ -3,92 +3,135 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 
-let camera, scene, renderer, stats;
-let controls, gui;
-let raycaster;
-let intersection = null;
-
-let pointer = new THREE.Vector2();
-
-const threshold = 0.1;
-const pointSize = 0.05;
-
-let planeGeometry, planeMaterial;
-
-init();
-animate();
-setupInitObjects();
-
-
-
 class Cube extends THREE.Mesh {
 	constructor() {
-		super()
-		this.geometry = new THREE.BoxGeometry();
-		this.material = new THREE.MeshStandardMaterial();
-		this.cubeSize = 0;
-		this.cubeActive = false;
+	  super()
+	  this.geometry = new THREE.BoxGeometry()
+	  this.material = new THREE.MeshStandardMaterial({ color: new THREE.Color('orange').convertSRGBToLinear() })
+	  this.cubeSize = 0
+	  this.cubeActive = false
 	}
-
+  
+	render() {
+	  
+	}
+  
+	onResize(width, height, aspect) {
+	  this.cubeSize = width / 5 // 1/5 of the full width
+	  this.scale.setScalar(this.cubeSize * (this.cubeActive ? 1.5 : 1))
+	}
+  
 	onPointerOver(e) {
-		this.material.color.set('orange');
-		this.material.color.convertSRGBToLinear();
+	  this.material.color.set('hotpink')
+	  this.material.color.convertSRGBToLinear()
 	}
-
+  
 	onPointerOut(e) {
-		this.material.color.set('white');
-		this.material.color.convertSRGBToLinear();
+	  this.material.color.set('orange')
+	  this.material.color.convertSRGBToLinear()
 	}
-
+  
 	onClick(e) {
-		this.cubeActive = !this.cubeActive;
-		
+	  this.cubeActive = !this.cubeActive
+	  this.scale.setScalar(this.cubeSize * (this.cubeActive ? 1.5 : 1))
 	}
-}
+  }
+  
+  // state
+  let width = 0
+  let height = 0
+  let intersects = []
+  let hovered = {}
+  
+  // setup
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  camera.position.set(10,10,10)
+  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+  renderer.setPixelRatio(Math.min(Math.max(1, window.devicePixelRatio), 2))
+  document.getElementById('root').appendChild(renderer.domElement)
+  const raycaster = new THREE.Raycaster()
+  const mouse = new THREE.Vector2()
 
 
+  // controls
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
 
+  
+  // view
+  const cube1 = new Cube()
+  cube1.position.set(-1.5, 0, 0)
+  scene.add(cube1)
 
-function init() {
-	scene = new THREE.Scene();
-
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.set( 10, 10, 10);
-	camera.lookAt(0, 0, 0);
-	camera.updateMatrix();
-
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
-	controls = new OrbitControls(camera, renderer.domElement);
-	controls.enableDamping = true;
-
-	raycaster = new THREE.Raycaster();
-	raycaster.params.Points.threshold = threshold;
-
-	stats = new Stats();
-	document.body.appendChild(stats.dom);
-
-	document.addEventListener('pointermove', onPointerMove);
-}
-
-
-
-function animate() {
-	requestAnimationFrame( animate );
-	render();
-	stats.update();
-}
-
-function render() {
-	raycaster.setFromCamera(pointer, camera);
-	const intersections = raycaster.intersectObjects(scene.children, false);
-	intersection = (intersections.length) > 0 ? intersections[0] : null;
-
-
-
-	renderer.render(scene, camera);
-	stats.update();
-}
-
+  const ambientLight = new THREE.AmbientLight()
+  const pointLight = new THREE.PointLight()
+  pointLight.position.set(10, 10, 10)
+  scene.add(ambientLight)
+  scene.add(pointLight)
+  
+  // responsive
+  function resize() {
+	width = window.innerWidth
+	height = window.innerHeight
+	camera.aspect = width / height
+	const target = new THREE.Vector3(0, 0, 0)
+	const distance = camera.position.distanceTo(target)
+	const fov = (camera.fov * Math.PI) / 180
+	const viewportHeight = 2 * Math.tan(fov / 2) * distance
+	const viewportWidth = viewportHeight * (width / height)
+	camera.updateProjectionMatrix()
+	renderer.setSize(width, height)
+	scene.traverse((obj) => {
+	  if (obj.onResize) obj.onResize(viewportWidth, viewportHeight, camera.aspect)
+	})
+  }
+  
+  window.addEventListener('resize', resize)
+  resize()
+  
+  // events
+  window.addEventListener('pointermove', (e) => {
+	mouse.set((e.clientX / width) * 2 - 1, -(e.clientY / height) * 2 + 1)
+	raycaster.setFromCamera(mouse, camera)
+	intersects = raycaster.intersectObjects(scene.children, true)
+  
+	// If a previously hovered item is not among the hits we must call onPointerOut
+	Object.keys(hovered).forEach((key) => {
+	  const hit = intersects.find((hit) => hit.object.uuid === key)
+	  if (hit === undefined) {
+		const hoveredItem = hovered[key]
+		if (hoveredItem.object.onPointerOver) hoveredItem.object.onPointerOut(hoveredItem)
+		delete hovered[key]
+	  }
+	})
+  
+	intersects.forEach((hit) => {
+	  // If a hit has not been flagged as hovered we must call onPointerOver
+	  if (!hovered[hit.object.uuid]) {
+		hovered[hit.object.uuid] = hit
+		if (hit.object.onPointerOver) hit.object.onPointerOver(hit)
+	  }
+	  // Call onPointerMove
+	  if (hit.object.onPointerMove) hit.object.onPointerMove(hit)
+	})
+  })
+  
+  window.addEventListener('click', (e) => {
+	intersects.forEach((hit) => {
+	  // Call onClick
+	  if (hit.object.onClick) hit.object.onClick(hit)
+	})
+  })
+  
+  // render-loop, called 60-times/second
+  function animate(t) {
+	requestAnimationFrame(animate)
+	scene.traverse((obj) => {
+	  if (obj.render) obj.render(t)
+	})
+	renderer.render(scene, camera)
+  }
+  
+  animate()
+  
